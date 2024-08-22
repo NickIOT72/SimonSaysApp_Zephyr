cbd9037490979cd5e5a2e82d4d0a73bb651cf628
fabocode <fabianbarraez@gmail.com>
2024-08-21 22:33:35 -0500

    Added handler for simon says command

--- src/modules/shell/shell.c
+++ src/modules/shell/shell.c
@@ -14,6 +14,20 @@ simon_says_opt_t shell_is_simon_enabled(void)
 	return simon.user_opt;
 }
 
+static int shell_cmd_simon_enable_handler(const struct shell *shell, size_t argc, char **argv)
+{
+	if ( argc != MINIMUM_MANDATORY_ENABLE_OPTIONS ){
+		shell_print(shell, "There must be 1 input (1 or 0). Try again.");
+		return -1;
+	}
+
+	const simon_says_opt_t NEW_STATE = atoi(argv[1]) == 1 ? ENABLED : DISABLED;
+	shell_set_simon(NEW_STATE);
+	shell_print(shell, "Simon Says %s.", NEW_STATE == ENABLED ? "Enabled" : "Disabled");
+
+    return 0;
+}
+
 SHELL_CMD_ARG_REGISTER(simon_enable, 
 	NULL, 
 	"By default, Simon Says is disabled, but you can enable it with a <1/0> input", 
